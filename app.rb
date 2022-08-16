7f4effee1a2c5e24d0515775076139e03f744aa2
{chirimirin} <hayatonakajo@HayatonoMacBookPro.local>
2022-08-16 17:44:26 +0900

    [Add] app

---
+++ app.rb
@@ -0,0 +1,22 @@
+def hantei(n)
+    num = 0
+    for i in 1..n + 1 do
+        d = n % i
+        if d == 0
+            num += 1
+        end
+    end
+    if num == 2 
+       result = true 
+    else
+        result = false
+    end
+end
+
+puts "素数かどうか判定したい整数を入力してください"
+value = gets
+if hantei(value.to_i)
+    puts "#{value} は素数です".gsub("\n","")
+else
+    puts "#{value} は素数ではありません".gsub("\n","")
+end
